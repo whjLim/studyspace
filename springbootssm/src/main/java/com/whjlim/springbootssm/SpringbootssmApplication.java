431e5aa7089dd44f8df39f3a9e5b65184b93dc92
whjLim <whjfzu@foxmail.com>
2019-04-16 21:41:38 +0800

    我的学习空间

---
+++ springbootssm/src/main/java/com/whjlim/springbootssm/SpringbootssmApplication.java
@@ -0,0 +1,13 @@
+package com.whjlim.springbootssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootssmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootssmApplication.class, args);
+	}
+
+}
