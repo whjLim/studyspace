c9e0c99fd93ef6e671ab45ff7402349486797f81
whjLim <514772362@qq.com>
2019-08-12 23:20:46 +0800

    创建线程---线程池初用

---
+++ BaseStudy/src/com/whjlim/thread/MyThread.java
@@ -0,0 +1,59 @@
+package com.whjlim.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by BlackWatch on 2019/8/12 22:08
+ *
+ * 创建线程的方式
+ */
+public class MyThread {
+
+    class TestThread extends Thread{
+        @Override
+        public void run() {
+            System.out.println("这是通过继承 Thread线程类创建线程");
+        }
+    }
+
+    class RunableTest implements Runnable{
+        @Override
+        public void run() {
+            System.out.println("通过实现 Runnable 接口创建线程");
+        }
+    }
+
+    class TestThreadPool{
+        public void testThreadPool(){
+            int count = 30;
+            ExecutorService threadPool = Executors.newFixedThreadPool(10);
+            while (count-->0){
+                threadPool.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(Thread.currentThread().getName() + "  is running ...");
+                        try {
+                            Thread.sleep(3000);
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+            threadPool.shutdown();
+        }
+
+    }
+
+    public void createThreadTest(){
+        TestThreadPool testThreadPool = new TestThreadPool();
+        testThreadPool.testThreadPool();
+    }
+
+    public static void main(String[] args) {
+       MyThread myThread = new MyThread();
+       myThread.createThreadTest();
+       System.out.println("线程池在干啥");
+    }
+}
