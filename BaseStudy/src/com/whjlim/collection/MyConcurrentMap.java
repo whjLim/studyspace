cd42a5c0b66585d5e668d10648b0e55436233129
whjLim <514772362@qq.com>
2019-08-18 01:42:32 +0800

    阅读学习理解jdk---ConcurrentHashMap

--- BaseStudy/src/com/whjlim/collection/MyConcurrentMap.java
+++ BaseStudy/src/com/whjlim/collection/MyConcurrentMap.java
@@ -1,6 +1,6 @@
 package com.whjlim.collection;
 
-import com.sun.javaws.exceptions.ErrorCodeResponseException;
+
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
@@ -54,25 +54,39 @@ public class MyConcurrentMap<K,V> {
 
     int initCapacity = 16;
     static long SIZECTL;
+    static long NUMS_BASIC;
+    static long NUMS_ASHIFT;
     int sizectl;
-    transient volatile Node<K,V>[] table;
-
+    transient volatile Node<K,V>[] table; //直接new Node<K,V>[16] 是不行的
+    transient volatile Integer[] nums = new Integer[20];
     void init(){
         int sc;
         while (table == null || table.length == 0){
             if((sc = sizectl) < 0)
                 Thread.yield();
             else if(unsafe.compareAndSwapInt(this,SIZECTL,sc,-1)){
+                int n = sc > 0 ? sc : 16;
 
             }
         }
     }
+
+    //通过这个方法能得到i, 线程安全吗
+    // 在获取头的时候，为何要用这个方法，不能直接数组取？
+    Integer getNums(Integer[] nums, int i){
+        return (Integer) unsafe.getObjectVolatile(nums, ((long)i<<NUMS_ASHIFT) + NUMS_BASIC);//((long)i << ASHIFT) + ABASE)
+    }
+
 
     static {
         try {
             Class<?> k = MyConcurrentMap.class;
             SIZECTL = unsafe.objectFieldOffset
                     (k.getDeclaredField("sizectl"));
+            Class<?> ak = Integer[].class;  //数组类型
+            NUMS_BASIC = unsafe.arrayBaseOffset(ak);
+            int scale = unsafe.arrayIndexScale(ak);//4
+            NUMS_ASHIFT = 31 - Integer.numberOfLeadingZeros(scale);//2
         }catch (Exception e){
             throw new Error(e);
         }
@@ -87,13 +101,61 @@ public class MyConcurrentMap<K,V> {
         //System.out.println(this);
     }
 
+    /**
+    *@Author: whjLim
+    *@Date: 0:46 2019/8/18
+    *测试一下Unsave的使用，更方便理解ConcurrentHashMap
+    */
+    public void testUnsave(){
+        MyConcurrentMap<String, String> myConcurrentMap = new MyConcurrentMap<>();
+        Thread threadRead = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    System.out.println(unsafe.compareAndSwapInt(myConcurrentMap,SIZECTL,myConcurrentMap.sizectl,-1));
+                }
+            }
+        });
+        Thread threadWrite = new Thread(new Runnable() {
+            int sc = 1;
+            @Override
+            public void run() {
+                while (true){
+                    System.out.println(myConcurrentMap.sizectl);
+                    myConcurrentMap.sizectl = sc++;
+                }
+            }
+        });
+
+        threadRead.start();
+        threadWrite.start();
+    }
+
 
     public static void main(String[] args) {
         HashMap<String,String> hashMap = new HashMap<>();
         ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
         MyConcurrentMap.Node<String,String> node = new MyConcurrentMap.Node<String,String>("whjLim".hashCode(),"whjLim","Good",null);
         MyConcurrentMap<String, String> myConcurrentMap = new MyConcurrentMap<>();
-        System.out.println(unsafe.compareAndSwapInt(myConcurrentMap,SIZECTL,myConcurrentMap.sizectl,-1));
+        //System.out.println(SIZECTL);
+        //System.out.println(NUMS_BASIC);
+        //System.out.println(unsafe.compareAndSwapInt(myConcurrentMap,SIZECTL,myConcurrentMap.sizectl,-1));
+        //myConcurrentMap.sizectl = 12;
+        //System.out.println(myConcurrentMap.sizectl);
+        myConcurrentMap.nums[15] = 1024;
+        System.out.println(myConcurrentMap.nums[15]);
+        System.out.println(myConcurrentMap.getNums(myConcurrentMap.nums,15));
+        for(int i = 0; i < 5; i++){
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true){
+
+                    }
+                }
+            });
+            thread.start();
+        }
 
     }
 }
